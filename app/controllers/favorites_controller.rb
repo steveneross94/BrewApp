1182866410d5dc3c20d083363ae5ecbeb0a9b2cb
steveneross94 <ross.steven@me.com>
2020-06-15 21:38:13 -0400

    favorite CRUD

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,42 @@
 class FavoritesController < ApplicationController
+    before_action :find_fav, only: [:show, :edit, :update, :delete]
+    def index
+        @favorites = Favorite.all
+    end
+    
+    def show
+    end
+
+    def new
+        @favorite = Favorite.new
+    end
+
+    def create
+        favorite = Favorite.create(fav_params)
+        redirect_to favorites_path
+    end
+
+    def edit
+    end
+
+    def update
+        @favorite.update
+        redirect_to favorite_path(@favorite)
+    end
+
+    def delete
+        @favorite.delete
+        redirect_to favorites_path
+    end
+
+    private
+
+    def fav_params
+        params.require(:favorite).permit(:brewery_id, :customer_id)
+    end
+
+    def find_fav
+        @favorite = Favorite.find(params[:id])
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 require 'json'
 
 Brewery.destroy_all
+Beer.destroy_all
+Customer.destroy_all
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -21,3 +23,16 @@ data.each do |brewery|
     
        Brewery.create(name: brewery["name"], state: brewery["state"], brewery_type: brewery["brewery_type"], website_url: brewery["website_url"])
 end
+
+##should name be username if we do beer-a-dex? can we create a leaderboard?
+Customer.create(name: "Steven Ross")
+Customer.create(name: "Andrew de la Vega")
+Customer.create(name: "Craig")
+Customer.create(name: "Sean")
+Customer.create(name: "Yoan Elon Space BBQ Musk Ante")
+
+Beer.create(name: "Test Beer", brewery: Brewery.first)
+
+Favorite.create(beer: Beer.first, customer: Customer.first)
+Favorite.create(beer: Beer.first, customer: Customer.third)
+
